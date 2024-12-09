eb6c4d90003b89a23f63f7aaae85cccce77e987b
ahsnghifari <ahsanghifar04@gmail.com>
2024-12-09 14:53:03 +0700

    Selesai

--- app/beranda/page.js
+++ app/beranda/page.js
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { useRouter } from "next/router";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 // KOMPONEN KAMI
